d2b460269b0fecafd846b5e5f51fb22222b99b71
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-07-24 22:56:13 +0100

    initial homepage

--- src/clj/elo/api.clj
+++ src/clj/elo/api.clj
@@ -5,9 +5,41 @@
             [environ.core :refer [env]]
             [compojure.core :refer [defroutes GET POST]]
             [elo.db :refer [store]]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [hiccup.core :as hiccup])
+  (:import (java.util UUID)))
 
 (def ^:private default-port 3000)
+
+(defn- cache-buster
+  [path]
+  ;; fallback to a random git sha when nothing is found
+  (format "%s?git_sha=%s"
+          path
+          (:heroku-slug-commit env (str (UUID/randomUUID)))))
+
+
+(def body
+  [:html
+   [:head [:meta {:charset "utf-8"
+                  :description "FIFA championship little helper"}]
+    [:title "FIFA championship"]
+
+    [:link {:href (cache-buster "css/screen.css")
+            :rel "stylesheet"
+            :type "text/css"}]]
+
+   [:body
+    [:div {:id "root"}
+     [:p "Enter here the results of the game"]]]])
+
+(defn home
+  []
+  (resp/content-type
+   (resp/response
+    (hiccup/html body))
+
+   "text/html"))
 
 (defn- get-port
   []
@@ -15,6 +47,7 @@
 
 (defroutes app-routes
   (POST "/store" request ())
+  (GET "/" [] (home))
   #_(GET "/" request (enter-page request)))
 
 (def app
