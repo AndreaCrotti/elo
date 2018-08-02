14c156cacbf934d49a9c1c97d090c2688de5d1da
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-08-02 21:55:24 +0100

    code to import from a real CSV

--- src/clj/elo/db.clj
+++ src/clj/elo/db.clj
@@ -1,9 +1,14 @@
 (ns elo.db
   (:require [clojure.java.jdbc :as jdbc]
+            [clj-time.format :as f]
+            [clj-time.coerce :as tc]
+            [clojure.data.csv :as csv]
             [environ.core :refer [env]]
             [honeysql.core :as sql]
             [honeysql.helpers :as h]
             [elo.helpers :refer [*test-db* test-db-uri]]))
+
+(def local-db "postgres://elo@localhost:5445/elo")
 
 (defn db-spec
   []
@@ -15,15 +20,24 @@
   (-> (h/insert-into :game)
       (h/values [params])))
 
+(defn conform
+  [data]
+  (-> data
+      (update :p1-goals #(Integer. %))
+      (update :p2-goals #(Integer. %))))
+
+(defn conform-with-date
+  [data]
+  (-> data
+      conform
+      (update :played-at
+              #(tc/to-sql-time (f/parse
+                                (f/formatter "DD/MM/yyyy HH:mm:ss") %)))))
+
 (defn store
   [params]
-  (let [new-params
-        (-> params
-            (update :p1-goals #(Integer. %))
-            (update :p2-goals #(Integer. %)))]
-
-    (jdbc/execute! (db-spec)
-                   (sql/format (store-sql new-params)))))
+  (jdbc/execute! (db-spec)
+                 (sql/format (store-sql (conform params)))))
 
 (defn- load-games-sql
   []
@@ -34,3 +48,32 @@
   []
   (jdbc/query (db-spec)
               (sql/format (load-games-sql))))
+
+(defn insert-game-sql
+  [values]
+  (-> (h/insert-into :game)
+      (h/values values)))
+
+(defn- import-csv
+  [filename]
+  (let [content (csv/read-csv (slurp filename))
+        strip-header (rest content)
+        parsed
+        (for [[played-at p1-name p2-name p1-goals p2-goals p1-team p2-team] strip-header]
+          {:p1-name p1-name
+           :p2-name p2-name
+           :p1-goals p1-goals
+           :p2-goals p2-goals
+           :p1-team p1-team
+           :p2-team p2-team
+           :played-at played-at})]
+
+    (jdbc/execute! local-db ;;(db-spec)
+                   (sql/format (insert-game-sql
+                                (map conform-with-date parsed))))))
+
+(defn -main
+  [& [filename]]
+  (import-csv filename))
+
+;; lein run -m elo.db sample.csv
