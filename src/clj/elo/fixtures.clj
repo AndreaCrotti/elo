33009bd8b667a2f68165b74aa6b13f417d751022
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-08-11 21:26:53 +0100

    add fixtures

--- src/clj/elo/fixtures.clj
+++ src/clj/elo/fixtures.clj
@@ -3,11 +3,33 @@
             [clojure.spec.gen.alpha :as g]))
 
 (s/def ::id uuid?)
+(s/def ::name string?)
+(s/def ::email string?)
+(s/def ::player (s/keys :req-un [::id
+                                 ::name
+                                 ::email]))
+
+(s/def ::p1 uuid?)
+(s/def ::p2 uuid?)
+(s/def ::p1_team string?)
+(s/def ::p2_team string?)
 (s/def ::p1_goals (s/int-in 0 10))
 (s/def ::p2_goals (s/int-in 0 10))
 
-(s/def ::team-name string?)
+;;TODO: add recorded and played_at
 
-(s/def ::player-name string?)
+(s/def ::game (s/keys :req-un [::id
+                               ::p1
+                               ::p2
+                               ::p1_team
+                               ::p2_team
+                               ::p1_goals
+                               ::p2_goals]))
 
-(s/def ::player-email string?)
+(defn game-gen
+  []
+  (s/gen ::game))
+
+(defn player-gen
+  []
+  (s/gen ::player))
