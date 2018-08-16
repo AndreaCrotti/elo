674b185dcf60615fa517eeb2f8d2e5856aa10467
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-08-16 20:59:54 +0100

    more css

--- src/clj/elo/api.clj
+++ src/clj/elo/api.clj
@@ -55,6 +55,10 @@
             :rel "stylesheet"
             :type "text/css"}]
 
+    [:link {:href (cache-buster "css/react-datepicker-cssmodules.css")
+            :rel "stylesheet"
+            :type "text/css"}]
+
     [:link {:rel "stylesheet"
             :href "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
             :integrity "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
