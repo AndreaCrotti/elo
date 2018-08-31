ae7839b9fdc87423e540f90149609b4bb8c63993
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-08-31 11:13:12 +0100

    try to make it backward compatible

--- resources/migrations/20180831090306-sf-support.up.sql
+++ resources/migrations/20180831090306-sf-support.up.sql
@@ -15,3 +15,13 @@ RENAME p1_goals to p1_points;
 
 ALTER TABLE game
 RENAME p2_goals to p2_points;
+
+
+CREATE TYPE game_type AS ENUM ('fifa', 'street-fighter');
+
+ALTER TABLE league
+ADD COLUMN game_type game_type NOT NULL default 'fifa';
+
+-- in case we want to store what was the version of the game played as well
+ALTER TABLE league
+ADD COLUMN game_version VARCHAR;
