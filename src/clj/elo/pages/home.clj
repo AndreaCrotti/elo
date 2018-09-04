5c6d5216a9084ae84d6c8678a944b01d7d48b99f
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-09-04 23:11:48 +0100

    initial playing around with plotly

--- src/clj/elo/pages/home.clj
+++ src/clj/elo/pages/home.clj
@@ -7,6 +7,7 @@
   [:html
    (gen-header "League page")
    [:body
+    [:div {:id "plot"} "Space for the plot is here"]
     [:div {:id "app"}]
     [:script {:src (cache-buster "js/compiled/app.js")}]
     [:script "elo.core.init();"]]])
