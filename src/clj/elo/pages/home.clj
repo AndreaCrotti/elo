6746eba7002f75a51cbbf9eabe9a9290e7e520b8
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-09-06 19:40:53 +0100

    cleanup

--- src/clj/elo/pages/home.clj
+++ src/clj/elo/pages/home.clj
@@ -7,12 +7,12 @@
   [:html
    (gen-header "League page")
    [:body
-    [:div.plots
-     [:div {:id "plot-cljs"} "Space for the plot is here"]
-     [:div {:id "plot-js"} "Space for the pure JS plot is here"]]
+    #_[:div.plots
+       [:div {:id "plot-cljs"} "Space for the plot is here"]
+       [:div {:id "plot-js"} "Space for the pure JS plot is here"]]
 
-    #_[:div {:id "app"}]
-    #_[:script {:src (cache-buster "js/compiled/app.js")}]
+    [:div {:id "app"}]
+    [:script {:src (cache-buster "js/compiled/app.js")}]
     [:script {:src (cache-buster "js/playground.js")}]
 
-    #_[:script "elo.core.init();"]]])
+    [:script "elo.core.init();"]]])
