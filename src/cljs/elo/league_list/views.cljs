1351889abfcfa7c378a55c8697b147f5ef19c0a5
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-09-09 20:12:14 +0100

    avoid infinite load-leagues loop

--- src/cljs/elo/league_list/views.cljs
+++ src/cljs/elo/league_list/views.cljs
@@ -6,16 +6,17 @@
 
 (defn root
   []
-  (rf/dispatch-sync [:load-leagues])
-  (let [leagues (rf/subscribe [:leagues])]
-    [:div.league__content
-     [:div.language_pick "Pick your League"]
-     (into [:ul.list-group]
-           (for [{:keys [id name game_type]} @leagues]
-             [:li.list-group-item
-              [:img.league_logo_small {:width "30px"
-                                       :src (config/logo (keyword game_type))}]
+  (rf/dispatch [:load-leagues])
+  (fn []
+    (let [leagues (rf/subscribe [:leagues])]
+      [:div.league__content
+       [:div.language_pick "Pick your League"]
+       (into [:ul.list-group]
+             (for [{:keys [id name game_type]} @leagues]
+               [:li.list-group-item
+                [:img.league_logo_small {:width "30px"
+                                         :src (config/logo (keyword game_type))}]
 
-              [:button {:on-click #(accountant/navigate!
-                                    (routes/path-for :league-detail :league-id id))}
-               name]]))]))
+                [:button {:on-click #(accountant/navigate!
+                                      (routes/path-for :league-detail :league-id id))}
+                 name]]))])))
