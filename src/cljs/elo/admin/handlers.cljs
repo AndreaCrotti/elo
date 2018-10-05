16b8607569a1dadda017ac6efec4bba8ca9538de
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-10-05 12:19:59 +0100

    use namespaced keywords

--- src/cljs/elo/admin/handlers.cljs
+++ src/cljs/elo/admin/handlers.cljs
@@ -21,31 +21,31 @@
 (rf/reg-event-db :email (setter [:player :email]))
 (rf/reg-event-db :league (setter [:league]))
 
-(rf/reg-sub :league (getter [:league]))
-(rf/reg-sub :leagues (getter [:leagues]))
-(rf/reg-sub :player (getter [:player]))
+(rf/reg-sub ::league (getter [:league]))
+(rf/reg-sub ::leagues (getter [:leagues]))
+(rf/reg-sub ::player (getter [:player]))
 
-(rf/reg-sub :valid-player?
+(rf/reg-sub ::valid-player?
             (fn [db _]
               (not-any? #(= % "")
                         (vals (common/get-in* db page [:player])))))
 
-(rf/reg-event-fx :add-player-success (fn [{:keys [db]} _]
+(rf/reg-event-fx ::add-player-success (fn [{:keys [db]} _]
                                        (js/alert "Thanks")))
 
-(rf/reg-event-db :reset-player (fn [db _]
-                                 (common/assoc-in* db page [:player] default-player)))
+(rf/reg-event-db ::reset-player (fn [db _]
+                                  (common/assoc-in* db page [:player] default-player)))
 
 (defn player-transform
   [db]
   (common/get-in* db page [:player]))
 
-(rf/reg-event-fx :add-player (common/writer page "/api/add-player"
-                                            :add-player-success player-transform))
+(rf/reg-event-fx ::add-player (common/writer page "/api/add-player"
+                                             :add-player-success player-transform))
 
-(rf/reg-event-db :load-leagues-success (setter [:leagues]))
+(rf/reg-event-db ::load-leagues-success (setter [:leagues]))
 
-(rf/reg-event-db :load-leagues (common/loader page "/api/leagues" :load-leagues-success))
+(rf/reg-event-db ::load-leagues (common/loader page "/api/leagues" ::load-leagues-success))
 
 (rf/reg-event-db ::initialize-db
                  (fn [db _]
--- src/cljs/elo/admin/views.cljs
+++ src/cljs/elo/admin/views.cljs
@@ -1,15 +1,17 @@
 (ns elo.admin.views
   (:require [re-frame.core :as rf]
+            [elo.admin.handlers :as handlers]
             [elo.common.views :refer [drop-down]]
             [elo.utils :as utils]))
 
 (defn add-player-form
   []
-  (let [valid-player? (rf/subscribe [:valid-player?])
-        player (rf/subscribe [:player])
-        league (rf/subscribe [:league])
-        leagues (rf/subscribe [:leagues])]
+  (let [valid-player? (rf/subscribe [::handlers/valid-player?])
+        player (rf/subscribe [::handlers/player])
+        league (rf/subscribe [::handlers/league])
+        leagues (rf/subscribe [::handlers/leagues])]
 
+    (js/console.log "Leagues are now =" @leagues)
     [:div.form-group.add-player_form
      [:div
       [drop-down @leagues :league]
@@ -31,14 +33,14 @@
                 :name "submit-game"
                 :class (utils/classes ["submit__game" "btn" "btn-primary" (when-not @valid-player? "disabled")])
                 :on-click (if @valid-player?
-                            #(rf/dispatch [:add-player])
+                            #(rf/dispatch [::handlers/add-player])
                             #(js/alert "Fill up the form first"))}
 
        "Register New Player"]]]))
 
 (defn root
   []
-  (rf/dispatch [:load-leagues])
+  (rf/dispatch-sync [::handlers/load-leagues])
   (fn []
     [:div.admin__page
      [add-player-form]]))
