f13a728999acec5e07e93612b5a819f4412ad6a0
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-10-07 18:57:18 +0100

    integrate Klipse

--- src/clj/elo/pages/home.clj
+++ src/clj/elo/pages/home.clj
@@ -2,17 +2,25 @@
   (:require [elo.pages.utils :refer [cache-buster]]
             [elo.pages.header :refer [gen-header]]))
 
+(def klipse-css-href "https://storage.googleapis.com/app.klipse.tech/css/codemirror.css")
+
+(def klipse-selector
+  "window.klipse_settings = {
+        selector: '.language-klipse'
+    };")
+
+(def klipse-plugin-href "https://storage.googleapis.com/app.klipse.tech/plugin/js/klipse_plugin.js")
+
 (defn body
   []
   [:html
    (gen-header "League page")
+   [:link {:rel "stylesheet" :type "text/css" :href klipse-css-href}]
    [:body
-    #_[:div.plots
-       [:div {:id "plot-cljs"} "Space for the plot is here"]
-       [:div {:id "plot-js"} "Space for the pure JS plot is here"]]
-
     [:div {:id "app"}]
     [:script {:src (cache-buster "/cljs-out/elo-main.js")}]
     #_[:script {:src (cache-buster "js/playground.js")}]
 
+    [:script klipse-selector]
+    [:script {:src klipse-plugin-href}]
     [:script "elo.core.init();"]]])
