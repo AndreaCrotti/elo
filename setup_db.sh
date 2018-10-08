ecded806f0d0bdafd659bd0e68103bdbf7f3f316
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-10-08 17:12:18 +0100

    fix script

--- setup_db.sh
+++ setup_db.sh
@@ -5,9 +5,9 @@ set -ex
 DEV=" -h localhost -p 5445 -U elo elo"
 TEST="-h localhost -p 5445 -U elo elo_test"
 
-dropdb $DEV; createdb $DEV
+dropdb --if-exists $DEV; createdb $DEV
 
-dropdb $TEST; createdb $TEST
+dropdb --if-exists $TEST; createdb $TEST
 
 DATABASE_URL="postgres://elo@localhost:5445/elo" lein migratus migrate
 DATABASE_URL="postgres://elo@localhost:5445/elo_test" lein migratus migrate
