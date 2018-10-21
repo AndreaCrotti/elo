72c71cc356743589db0bea7adf25cf635d6cb12d
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-10-21 22:57:29 +0100

    start a stateful handler

--- src/cljs/elo/vega.cljs
+++ src/cljs/elo/vega.cljs
@@ -1,7 +1,24 @@
 (ns elo.vega
-  (:require [cljsjs.vega]))
+  (:require [cljsjs.vega]
+            [reagent.core :as reagent]))
 
 (def schema-url "https://vega.github.io/schema/vega-lite/v2.json")
+
+(defn vega-view
+  []
+  [:div.rankings-over-time
+   [:h4 "Rankings Over Time"]
+   [:div.rankings-over-time__visualization {:style {:height "400px"}}]])
+
+(defn vega-inner
+  []
+  (let [values (atom nil)
+        update (fn [comp]
+                 [])]
+    (reagent/create-class
+     {:reagent-render vega-view
+      :component-did-mount 1
+      :display-name "Rankings Over Time Inner"})))
 
 (defn rankings-vega-definition
   [values]
