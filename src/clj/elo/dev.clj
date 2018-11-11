09221a5bdb0f4c602bb11baad2356abd96b72638
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-11-11 17:59:18 +0000

    first fully working garden/figwheel/jetty integrant

--- src/clj/elo/dev.clj
+++ src/clj/elo/dev.clj
@@ -1,32 +1,53 @@
 (ns elo.dev
-  (:require [integrant.core :as ig]
-            [clojure.java.io :as io]
-            [garden.compiler :refer [compile-css]]
-            [ring.adapter.jetty :as jetty]
+  (:require [clojure.java.io :as io]
             [elo.api :as api]
             [figwheel.main.api :as figwheel]
-            [ring.middleware.reload :refer [wrap-reload]])
+            [garden.compiler :refer [compile-css]]
+            [integrant.core :as ig]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [taoensso.timbre :as log])
 
   (:gen-class))
 
 (defonce system (atom {}))
 
-(defmethod ig/init-key :server/garden
-  [_ {:keys [builds]}]
-  (doseq [{:keys [compiler stylesheet id]} builds]
-    (.mkdirs (.getParentFile (io/file (:output-to compiler))))
-    (require (symbol (namespace stylesheet)) :reload)
-    (compile-css compiler (var-get (resolve stylesheet))))
+(def garden-options
+  [{:id "screen"
+    :source-paths ["src/clj" "src/cljc"]
+    :stylesheet "elo.css"
+    :compiler {:output-to "resources/public/css/screen.css"
+               :pretty-print? true}}])
 
+(defn start-garden
+  [builds]
+  (doseq [{:keys [compiler stylesheet id]} builds]
+    (log/info (str "Loading build " id))
+    (.mkdirs (.getParentFile (io/file (:output-to compiler))))
+    (require (symbol stylesheet) :reload)
+    (log/infof "writing to file %s" (:output-to compiler))
+    (compile-css compile-css
+                 (var-get (get (ns-map (symbol stylesheet))
+                               (symbol id)))))
   (mapv #(get-in % [:compiler :output-to]) builds))
+
+(defmethod ig/init-key :server/garden
+  [_ _]
+  (log/info "Starting Garden")
+  (start-garden garden-options))
+
+(defmethod ig/halt-key! :server/garden
+  [_ output-files]
+  (log/info "stopping Garden"))
 
 (defmethod ig/init-key :server/figwheel
   [_ {:keys [build] :as opts}]
+  (log/info "Starting Figwheel")
   (figwheel/start build))
 
 (defmethod ig/init-key :server/jetty
   [_ {:keys [port]}]
-  (println "Running jetty")
+  (log/info "Starting Jetty")
   (jetty/run-jetty (wrap-reload #'api/app)
                    {:join? false
                     :port port}))
@@ -35,11 +56,15 @@
   [_ server]
   (.stop server))
 
+(defmethod ig/init-key :server/nrepl
+  [_ {:keys [port]}]
+  (log/info "Starting Nrepl"))
+
 (def config
-  {;;:server/garden {:builds ["screen"]}
+  {:server/garden {}
    :server/figwheel {:build "elo"}
    :server/jetty {:port 3335}
-   })
+   :server/nrepl {}})
 
 (defn -main
   [& args]
