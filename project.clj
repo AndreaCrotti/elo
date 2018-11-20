6673d2bf8610b0716db7766251cbfde917cbbb7e
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-11-20 08:29:17 +0000

    try to disable optimizations

--- project.clj
+++ project.clj
@@ -140,6 +140,6 @@
      :compiler     {:main elo.core
                     :output-to "resources/public/cljs-out/elo-main.js"
                     :asset-path "resources/public/cljs-out/elo"
-                    :optimizations :simple
+                    :optimizations :none
                     :closure-defines {goog.DEBUG false}
                     :pretty-print false}}]})
