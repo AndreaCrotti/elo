dca5b2d15f63fe65b9e83d14638a8fd31b551dda
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-11-26 16:31:03 +0000

    distinguish between build types depending env var

--- src/clj/elo/pages/home.clj
+++ src/clj/elo/pages/home.clj
@@ -2,7 +2,14 @@
   (:require [elo.pages.utils :refer [cache-buster]]
             [elo.pages.header :refer [gen-header]]
             [elo.routes :as routes]
+            [environ.core :refer [env]]
             [clojure.data.json :as json]))
+
+(defn- build-filename
+  []
+  (if (= (:dev env) "true")
+    "/cljs-out/dev-main.js"
+    "/cljs-out/prod-main.js"))
 
 (defn client-side-config
   [request]
@@ -23,6 +30,6 @@
    [:body
     [:div {:id "app"}]
     [:script (format "window['config']=%s" (client-side-config request))]
-    [:script {:src (cache-buster "/cljs-out/prod-main.js")}]
+    [:script {:src (cache-buster (build-filename))}]
 
     [:script "elo.core.init();"]]])
