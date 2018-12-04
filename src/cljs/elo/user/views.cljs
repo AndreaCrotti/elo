b322c967f4544303f48bad5dd585830dc382a439
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-12-04 22:24:41 +0000

    missing file

---
+++ src/cljs/elo/user/views.cljs
@@ -0,0 +1,12 @@
+(ns elo.user.views)
+
+;; sections that can be useful to add
+;; - nemesis
+;; - head to head
+;; - suggested opponent
+;; - personal stats
+;; - best performing team
+
+(defn root
+  []
+  [:h2 "Hello Player detail page"])
