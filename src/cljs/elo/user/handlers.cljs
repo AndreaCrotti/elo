30d77d5b51a1272e70046b2c7830f3bbb6974c56
Andrea Crotti <andrea.crotti.0@gmail.com>
2018-12-31 07:25:25 +0000

    more events

--- src/cljs/elo/user/handlers.cljs
+++ src/cljs/elo/user/handlers.cljs
@@ -7,9 +7,18 @@
 
 (def page ::page-id)
 
+(rf/reg-sub ::player-id
+            (fn [db _]
+              (get-in db [:route-params :player-id])))
+
 ;;TODO: pass the spec
 (def setter (partial common/setter* page))
 (def getter (partial common/getter* page))
 
 (rf/reg-sub ::opponent (getter [:opponent]))
 (rf/reg-event-db ::opponent (setter [:opponent]))
+
+(rf/reg-event-db ::load-player-success (setter [:player]))
+
+;; now we can load all the player information, which includes the league
+(rf/reg-event-fx ::load-player (common/loader-no-league-id page "/api/player" ::load-player-success))
--- src/cljs/elo/user/views.cljs
+++ src/cljs/elo/user/views.cljs
@@ -1,6 +1,5 @@
 (ns elo.user.views
   (:require [re-frame.core :as rf]
-            [elo.league-detail.handlers :as detail-handlers]
             [elo.common.players :as players-handlers]
             [elo.common.views :as common-views]
             [elo.user.handlers :as handlers]))
@@ -12,20 +11,20 @@
 ;; - personal stats
 ;; - best performing team
 
-#_[drop-down-players sorted-players ::handlers/p1 (:p1 @game)
-   {:caption "Name"}]
-
 (defn head-to-head
   []
   (let [players (rf/subscribe [::players-handlers/players])
-        head-to-head-wins (rf/subscribe [::handlers/head-to-head-wins])]
+        ;; head-to-head-wins (rf/subscribe [::handlers/head-to-head-wins])
+]
     (fn []
       ;; show win draws and losses
       [:div.form-group.opponent__input
-       [common-views/drop-down-players players ::handlers/opponent]])))
+       [common-views/drop-down-players @players ::handlers/opponent
+        {:caption "Name"}]])))
 
 (defn root
   []
-  [:h2 "Hello Player detail page"
-   [:div.user__root
-    [head-to-head]]])
+  (fn []
+    [:h2 "Hello Player detail page"
+     [:div.user__root
+      [head-to-head]]]))
