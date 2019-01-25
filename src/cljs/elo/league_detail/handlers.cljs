11310ed13630a736ab5f3ca20d880b05571c5c77
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-01-25 16:48:57 +0000

    need to check the seq

--- src/cljs/elo/league_detail/handlers.cljs
+++ src/cljs/elo/league_detail/handlers.cljs
@@ -99,12 +99,13 @@
             :<- [::players-handlers/name-mapping]
 
             (fn [[games up-to-games name-mapping]]
-              (->> ((juxt :p1 :p2)
-                    ;; this should not be necessary
-                    (nth games (dec (or up-to-games
-                                        (count games)))))
-                   (map name-mapping)
-                   set)))
+              (when (seq games)
+                (->> ((juxt :p1 :p2)
+                      ;; this should not be necessary
+                      (nth games (dec (or up-to-games
+                                          (count games)))))
+                     (map name-mapping)
+                     set))))
 
 (rf/reg-sub ::last-ranking-changes-by-player
             :<- [::rankings-history]
