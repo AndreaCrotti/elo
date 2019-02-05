866132d737a41b4ec0a04f009f4b8b3f46ac21fe
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-05 21:33:44 +0000

    revert a couple of things

--- src/clj/elo/dev.clj
+++ src/clj/elo/dev.clj
@@ -1,8 +1,6 @@
 (ns elo.dev
-  (:require [clojure.java.io :as io]
-            [elo.api :as api]
+  (:require [elo.api :as api]
             [figwheel.main.api :as figwheel]
-            [garden.compiler :refer [compile-css]]
             [integrant.core :as ig]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
@@ -11,34 +9,6 @@
   (:gen-class))
 
 (defonce system (atom {}))
-
-(def garden-options
-  [{:id "screen"
-    :source-paths ["src/clj" "src/cljc"]
-    :stylesheet "elo.css"
-    :compiler {:output-to "resources/public/css/screen.css"
-               :pretty-print? true}}])
-
-(defn start-garden
-  [builds]
-  (doseq [{:keys [compiler stylesheet id]} builds]
-    (log/info (str "Loading build " id))
-    (.mkdirs (.getParentFile (io/file (:output-to compiler))))
-    (require (symbol stylesheet) :reload)
-    (log/infof "writing to file %s" (:output-to compiler))
-    (compile-css compile-css
-                 (var-get (get (ns-map (symbol stylesheet))
-                               (symbol id)))))
-  (mapv #(get-in % [:compiler :output-to]) builds))
-
-(defmethod ig/init-key :server/garden
-  [_ _]
-  (log/info "Starting Garden")
-  (start-garden garden-options))
-
-(defmethod ig/halt-key! :server/garden
-  [_ output-files]
-  (log/info "stopping Garden"))
 
 (defmethod ig/init-key :server/figwheel
   [_ {:keys [build] :as opts}]
@@ -61,8 +31,7 @@
   (log/info "Starting Nrepl"))
 
 (def config
-  {:server/garden {}
-   :server/figwheel {:build "elo"}
+  {:server/figwheel {:build "elo"}
    :server/jetty {:port 3335}
    :server/nrepl {}})
 
