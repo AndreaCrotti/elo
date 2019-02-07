9c4b6119885080d5daaaee08c1fe953359285e24
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-07 21:27:25 +0000

    fa -> elo

--- elo_auto_go.el
+++ elo_auto_go.el
@@ -1,20 +1,20 @@
 (require 'projectile)
 (require 'cider)
 
-(defun fa-clojure-repl? ()
+(defun elo-clojure-repl? ()
   (not (null (member cider-repl-type '(clj "clj")))))
 
-(defun fa-start-finops-admin ()
+(defun elo-start-finops-admin ()
   (interactive)
   (cider-switch-to-repl-buffer)
   (when
       (and
-       (fa-clojure-repl?)
+       (elo-clojure-repl?)
        (equal (projectile-project-name) "elo"))
 
     (insert "(ir/go)")
     (cider-repl-return)))
 
-(add-hook 'cider-connected-hook 'fa-start-finops-admin)
+(add-hook 'cider-connected-hook 'elo-start-finops-admin)
 
 (provide 'elo-auto-go)
