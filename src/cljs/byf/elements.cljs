5cdc7e4c2dc1561e302b2e7e774214a1cb861af4
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-16 16:34:44 +0000

    new sample element function

--- src/cljs/byf/elements.cljs
+++ src/cljs/byf/elements.cljs
@@ -1,6 +1,15 @@
 (ns byf.elements
   (:require [byf.utils :refer [classes]]))
 
+(defn el
+  [tag base-class]
+  (fn [cls args body]
+    [tag
+     (merge args
+            {:class (classes (conj cls base-class))})
+     (when (some? body)
+       body)]))
+
 (defn input
   [cls args]
   [:input
