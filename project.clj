1e9f7295725a5c6bb91b15fec6e207f8ddbeb8f0
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-22 21:51:30 +0000

    try to add kaocha cljs as well

--- project.clj
+++ project.clj
@@ -104,6 +104,7 @@
 
   :profiles
   {:kaocha {:dependencies [[lambdaisland/kaocha "0.0-389"]
+                           [lambdaisland/kaocha-cljs "0.0-16"]
                            [lambdaisland/kaocha-cloverage "0.0-22"]
                            [lambdaisland/kaocha-junit-xml "0.0-63"]]}
    :uberjar {:hooks []
