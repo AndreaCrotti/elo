48936d921067467692615dd6d5c0a59c833fa093
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-06-23 15:55:46 +0100

    bump clojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/spec.alpha "0.2.176"]
                  [clj-http "3.9.1"]
                  [prone "1.6.1"]
