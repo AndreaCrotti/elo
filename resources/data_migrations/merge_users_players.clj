f41ee8ad12f65228b88c75fbe9137dec559bbc1d
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-07-15 11:55:57 +0100

    initial implementation

--- resources/data_migrations/merge_users_players.clj
+++ resources/data_migrations/merge_users_players.clj
@@ -1,2 +1,13 @@
 (ns data-migrations.merge-users-players
-  (:require [byf.db :as db]))
+  (:require [byf.db :as db]
+            [clojure.java.jdbc :as jdbc]
+            [honeysql.helpers :as h]
+            [honeysql.core :as sql]))
+
+(defn merge-user-in-player
+  []
+  (doseq [p (db/query (fn [] (-> (h/select :*)
+                                (h/from :player))))]))
+
+(defn -main [& args]
+  (merge-user-in-player))
