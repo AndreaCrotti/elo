c008238c30b1b11ee96456d2ac136f2f3008ed16
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-07-27 13:34:31 +0100

    add data migration script

--- resources/data_migrations/merge_users_players.clj
+++ resources/data_migrations/merge_users_players.clj
@@ -4,10 +4,25 @@
             [honeysql.helpers :as h]
             [honeysql.core :as sql]))
 
+(defn up
+  [j]
+  (->
+   (h/update :player)
+   (h/sset (select-keys j [:email :created_at :active]))
+   (h/where [:= :player.user_id (:user_id j)])))
+
 (defn merge-user-in-player
   []
-  (doseq [p (db/query (fn [] (-> (h/select :*)
-                                (h/from :player))))]))
+  (doseq [joined
+          (db/query (fn [] (-> (h/select :p.user_id :u.id :u.email :u.created_at :u.active)
+                              (h/from [:player :p])
+                              (h/join [:users :u]
+                                      [:= :u.id :p.user_id]))))]
+    (let [upd (sql/format (up joined))]
+      (println "Running " upd)
+      (jdbc/execute! (db/db-spec)
+                     upd))))
+
 
 (defn -main [& args]
   (merge-user-in-player))
