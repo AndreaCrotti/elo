9fc707a6b7a1ab8d7f15ecd5f64d213d2cb940d5
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-09-01 09:13:28 +0100

    fix day

--- src/cljs/byf/league_detail/games_list.cljs
+++ src/cljs/byf/league_detail/games_list.cljs
@@ -1,11 +1,13 @@
 (ns byf.league-detail.games-list
   (:require [re-frame.core :as rf]
             [byf.common.players :as players-handlers]
-            [byf.common.views :as common-views]
-            [byf.league-detail.utils :refer [enumerate format-date]]
+            [byf.league-detail.utils :refer [enumerate]]
             [byf.league-detail.handlers :as handlers]
-            [antizer.reagent :as ant]
-            [reagent.core :as r]))
+            [antizer.reagent :as ant]))
+
+(defn- format-date
+  [timestamp]
+  (.format (js/moment timestamp) "YYYY-MM-DD"))
 
 (def columns
   [{:title "game #"
@@ -30,22 +32,21 @@
     :dataIndex :p2_points}
 
    {:title "day"
-    :dataindex :played_at}])
+    :dataIndex :played_at}])
 
 (defn rows
   []
   (let [games @(rf/subscribe [::handlers/games-live-players])
         name-mapping @(rf/subscribe [::players-handlers/name-mapping])
-        ;; up-to (rf/subscribe [::handlers/up-to-games])
-        ;; show-all? (rf/subscribe [::handlers/show-all?])
         rev-games (-> games enumerate reverse)]
 
     (for [[idx {:keys [p1 p2] :as g}] rev-games]
       (merge
        (select-keys g
-                    [:p1_using :p2_using :p1_points :p2_points :played_at])
+                    [:p1_using :p2_using :p1_points :p2_points])
        {:player-1 (get name-mapping p1)
         :player-2 (get name-mapping p2)
+        :played_at (format-date (:played_at g))
         :game-idx idx}))))
 
 (defn games-table
