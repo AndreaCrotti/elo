2980bcbff23f0bab777b2cd2f1913424a13bf7d6
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-12-11 11:30:04 +0000

    type migration

--- resources/migrations/20191211111544-game-type-table.up.sql
+++ resources/migrations/20191211111544-game-type-table.up.sql
@@ -1,7 +1,12 @@
 CREATE TABLE game_kind (
-       name VARCHAR(16) PRIMARY KEY,
+       id UUID PRIMARY KEY,
+       name VARCHAR(16) NOT NULL,
+       variant VARCHAR(16),
        draw_allowed BOOLEAN NOT NULL,
        team_required BOOLEAN NOT NULL,
        min_points INTEGER DEFAULT 0,
        max_points INTEGER DEFAULT 10
 );
+
+ALTER TABLE league
+ADD COLUMN game_kind VARCHAR(16) REFERENCES game_kind(name);
