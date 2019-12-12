1b33945093772fe34ffa5df79e7583da9c989a8a
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-12-12 10:04:16 +0000

    just use a json field

--- resources/migrations/20191211111544-game-type-table.up.sql
+++ resources/migrations/20191211111544-game-type-table.up.sql
@@ -1,12 +1,8 @@
 CREATE TABLE game_kind (
        id UUID PRIMARY KEY,
        name VARCHAR(16) NOT NULL,
-       variant VARCHAR(16),
-       draw_allowed BOOLEAN NOT NULL,
-       team_required BOOLEAN NOT NULL,
-       min_points INTEGER DEFAULT 0,
-       max_points INTEGER DEFAULT 10
+       config JSONB NOT NULL
 );
 
 ALTER TABLE league
-ADD COLUMN game_kind VARCHAR(16) REFERENCES game_kind(name);
+ADD COLUMN game_kind UUID REFERENCES game_kind(id);
