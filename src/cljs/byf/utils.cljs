d32748bbd5c55ac77f36f0f76b61a7d6a97861d3
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-02-26 15:12:04 +0000

    add comparator-by and some more css to improve theui

--- src/cljs/byf/utils.cljs
+++ src/cljs/byf/utils.cljs
@@ -1,6 +1,7 @@
 (ns byf.utils
   "Various utility functions for Clojurescript"
   (:require [clojure.string :as str]
+            [goog.object :as object]
             [re-frame.core :as rf]))
 
 (def min-width 500)
@@ -21,3 +22,9 @@
 (defn mobile?
   []
   (< js/window.screen.availWidth min-width))
+
+(defn comparator-by
+  "Compares two objects based on field"
+  [field]
+  #(compare (object/get %1 (name field))
+            (object/get %2 (name field))))
